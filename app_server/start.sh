e0b59db729effa6675ef329debc655a8927b0c2a
Inoue Shinichi Macbook Pro 2016 <inoue.shinichi.1800@gmail.com>
2023-11-19 21:59:59 +0900

    [Update] 127.0.0.1 -> 0.0.0.0

--- app_server/start.sh
+++ app_server/start.sh
@@ -8,4 +8,4 @@ echo "[Check \$HOST_WEB_DOMAIN] $HOST_WEB_DOMAIN"
 echo "[Ckeck \$EXPORT_APP_HTTP_PORT] $EXPORT_APP_HTTP_PORT"
 
 # exec gunicorn $DJANGO_PROJECT_NAME.wsgi --bind="http://$HOST_WEB_DOMAIN:$EXPORT_APP_HTTP_PORT"
-exec gunicorn $DJANGO_PROJECT_NAME.wsgi --bind="127.0.0.1:$EXPORT_APP_HTTP_PORT"
+exec gunicorn $DJANGO_PROJECT_NAME.wsgi --bind="0.0.0.0:$EXPORT_APP_HTTP_PORT"
