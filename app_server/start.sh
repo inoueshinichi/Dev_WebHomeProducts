26d9db6b7da444563fb1dbfb6486c28d872ceebd
Inoue Shinichi Macbook Pro 2016 <inoue.shinichi.1800@gmail.com>
2023-11-19 22:15:12 +0900

    [Update] hostname

--- app_server/start.sh
+++ app_server/start.sh
@@ -5,7 +5,7 @@ echo "[Check \$WSGI_UNIX_SOCKET_FILE] $WSGI_UNIX_SOCKET_FILE"
 # exec gunicorn $DJANGO_PROJECT_NAME.wsgi --bind="unix:$WSGI_UNIX_SOCKET_FILE"
 
 echo "[Check \$HOST_WEB_DOMAIN] $HOST_WEB_DOMAIN"
-echo "[Ckeck \$EXPORT_APP_HTTP_PORT] $EXPORT_APP_HTTP_PORT"
+echo "[Ckeck \$NAIVE_APP_HTTP_PORT] $NAIVE_APP_HTTP_PORT"
 
-# exec gunicorn $DJANGO_PROJECT_NAME.wsgi --bind="http://$HOST_WEB_DOMAIN:$EXPORT_APP_HTTP_PORT"
-exec gunicorn $DJANGO_PROJECT_NAME.wsgi --bind="0.0.0.0:$EXPORT_APP_HTTP_PORT"
+exec gunicorn $DJANGO_PROJECT_NAME.wsgi --bind="http://$HOST_WEB_DOMAIN:$NAIVE_APP_HTTP_PORT"
+# exec gunicorn $DJANGO_PROJECT_NAME.wsgi --bind="0.0.0.0:$EXPORT_APP_HTTP_PORT"
